0790bfeb51c43d51eb5672e41ace008505ffcd97
West <micwes2874@gmail.com>
2023-02-15 16:49:04 -0500

    Script to install all required modules for Alta3 courses

---
+++ _installAllModules.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Requests'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'OAuth'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Flask'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyyaml'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'paramiko'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ansible'])
