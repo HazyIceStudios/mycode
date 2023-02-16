a44f0f1c68fc2d3525bc7edced9b6ab20689002e
West <micwes2874@gmail.com>
2023-02-16 12:39:28 -0500

    Update _installAllModules.py
    added more modules

--- _installAllModules.py
+++ _installAllModules.py
@@ -2,14 +2,14 @@ import sys
 import subprocess
 
 # implement pip as a subprocess:
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Requests'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'OAuth'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Flask'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Requests'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'OAuth'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Flask'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Flask-Limiter'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyyaml'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'paramiko'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ansible'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyyaml'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'paramiko'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ansible'])
 # subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sys'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'os'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'os'])
